c2bbbcf7d97181c6d2abbec0b31a0e3d3cd75013
Alejandra Gabriela Insaurralde <alejainsaurralde@gmail.com>
2024-04-04 20:02:26 -0300

    configuracion de usuario

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 
+
+
+
 namespace Dominio
 {
      public class Articulo
@@ -14,11 +17,12 @@ namespace Dominio
         public string Codigo { get; set; } 
         public string Nombre { get; set; }
         [DisplayName("Descripción")]
-        public string Descripcion { get; set; } 
+        public string Descripcion { get; set; }
         public string ImagenUrl { get; set; }
         public string Marca { get; set; }
+
         [DisplayName("Categoría")]
-        public string Categoria { get; set; }   
+        public string Categoria { get; set; }
         public decimal Precio { get; set; }
-     }
+    }
 }
